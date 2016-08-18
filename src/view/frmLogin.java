23b7642c22387fc356b35544b80df34475636b34
hromao <hromao14@gmail.com>
2016-08-18 10:44:19 -0300

    Optimized Imports, removed useless comments

--- src/view/frmLogin.java
+++ src/view/frmLogin.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
 import dao.DaoLogin;
-import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.sql.SQLException;
-import java.text.ParseException;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
