03dd5d1c165ccb08f47ff6dbe3bd67cae85ffaae
hromao <hromao14@gmail.com>
2016-08-18 12:36:04 -0300

    Merge master into HEAD

--- src/dao/DaoCliente.java
+++ src/dao/DaoCliente.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import model.Clientes;
-import dao.Conexao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
